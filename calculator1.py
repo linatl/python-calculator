d1773e4c943696da01006a12706164bbc4690a9c
linatl <47339295+linatl@users.noreply.github.com>
2021-09-09 21:04:22 +0200

    Update calculator1.py
    Deleted some of the too many blank lines

--- calculator1.py
+++ calculator1.py
@@ -25,7 +25,6 @@ def result():
     e.delete(0, END)
     e.insert(0, result)
 
-
 # Make the buttons
 button_clear = Button(root, text="Clear", width=4, height=1, command=clear)
 button_left_parenthesis = Button(root, text="(", width=4, height=1, command=lambda: click("("))
@@ -37,23 +36,19 @@ button_8 = Button(root, text="8", width=4, height=1, command=lambda: click(8))
 button_9 = Button(root, text="9", width=4, height=1, command=lambda: click(9))
 button_multiply = Button(root, text="*", width=4, height=1, command=lambda: click("*"))
 
-
 button_4 = Button(root, text="4", width=4, height=1, command=lambda: click(4))
 button_5 = Button(root, text="5", width=4, height=1, command=lambda: click(5))
 button_6 = Button(root, text="6", width=4, height=1, command=lambda: click(6))
 button_subtract = Button(root, text="-", width=4, height=1, command=lambda: click("-"))
-
 
 button_1 = Button(root, text="1", width=4, height=1, command=lambda: click(1))
 button_2 = Button(root, text="2", width=4, height=1, command=lambda: click(2))
 button_3 = Button(root, text="3", width=4, height=1, command=lambda: click(3))
 button_add = Button(root, text="+", width=4, height=1, command=lambda: click("+"))
 
-
 button_0 = Button(root, text="0", width=4, height=1, command=lambda: click(0))
 button_separator = Button(root, text=",", width=4, height=1, command=lambda: click("X"))
 button_equal = Button(root, text="=", width=12, height=1, command=result)
-
 
 # Put the buttons on the screen
 
@@ -80,6 +75,5 @@ button_add.grid(row=4, column=3)
 button_0.grid(row=5, column=0)
 button_separator.grid(row=5, column=1)
 button_equal.grid(row=5, column=2, columnspan=2)
-
 
 root.mainloop()
